514acf790d6568c936c8c98f595b95932806fa9b
joseka1234 <joseka1234@gmail.com>
2020-04-30 21:01:16 +0100

    Dash arreglado

--- Assets/Scripts/TopDownPlayerController.cs
+++ Assets/Scripts/TopDownPlayerController.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 
 public enum PlayerState
 {
-    IDLE, CaminarDown, CaminarUp, CaminarLeft, CaminarRight, Correr, Dash, Dashing
+    IDLE, CaminarDown, CaminarUp, CaminarLeft, CaminarRight,
+    AtacarDown, AtacarUp, AtacarLeft, AtacarRight,
+    Correr, Dashing
 }
 
 public class TopDownPlayerController : MonoBehaviour
@@ -12,9 +14,12 @@ public class TopDownPlayerController : MonoBehaviour
     public float FuerzaDash;
 
     private const float MIN_DELTA = 0.2f;
+    private const float MAX_DASH_COOLDOWN = 2.0f;
+
     private bool Dashing;
     private Vector2 Movimiento;
     private Animator animator;
+    private float DashCooldown;
 
     private Rigidbody2D rigidbody2D;
     private PlayerState estado;
@@ -25,36 +30,41 @@ public class TopDownPlayerController : MonoBehaviour
         rigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         Dashing = false;
+        DashCooldown = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         Mover();
+        CheckState();
+    }
 
-        switch(estado)
+    private void CheckState()
+    {
+        switch (estado)
         {
             case PlayerState.IDLE:
-                animator.SetTrigger("IDLE");
                 break;
 
             case PlayerState.CaminarDown:
             case PlayerState.CaminarUp:
             case PlayerState.CaminarRight:
             case PlayerState.CaminarLeft:
-                animator.SetTrigger(estado.ToString());
                 rigidbody2D.velocity = Movimiento * Velocidad;
+                break;
+
+            case PlayerState.AtacarDown:
+            case PlayerState.AtacarUp:
+            case PlayerState.AtacarRight:
+            case PlayerState.AtacarLeft:
                 break;
 
             case PlayerState.Correr:
                 rigidbody2D.velocity = Movimiento * Velocidad;
                 break;
-
-            case PlayerState.Dash:
-                rigidbody2D.AddForce(Movimiento * FuerzaDash, ForceMode2D.Impulse);
-                estado = PlayerState.Dashing;
-                break;
         }
+        animator.SetTrigger(estado.ToString());
     }
 
     private void Mover()
@@ -63,13 +73,15 @@ public class TopDownPlayerController : MonoBehaviour
 
         if (estado != PlayerState.Dashing)
         {
-            if (Input.GetKey(KeyCode.J))
+            if (Input.GetKey(KeyCode.Joystick1Button0) && DashCooldown <= 0) // TODO: La comprobación del cooldown no funciona
+            //if (Input.GetKey(KeyCode.Joystick1Button0))
             {
+                rigidbody2D.AddForce(Movimiento * FuerzaDash, ForceMode2D.Impulse);
                 StartCoroutine(Dash());
                 return;
             }
 
-            if (Mathf.Abs(Movimiento.x + Movimiento.y) > MIN_DELTA)
+            if (GetMovimientoAbsoluto() > MIN_DELTA)
             {
                 CheckDireccionCaminar();
             }
@@ -98,14 +110,30 @@ public class TopDownPlayerController : MonoBehaviour
         {
             estado = PlayerState.CaminarDown;
         }
-
-        Debug.Log(estado.ToString());
     }
+
+    private float GetMovimientoAbsoluto()
+    {
+        return Mathf.Abs(Movimiento.x) + Mathf.Abs(Movimiento.y);
+    }
+
+    private float GetVelocidadAbsoluta()
+    {
+        return Mathf.Abs(rigidbody2D.velocity.x) + Mathf.Abs(rigidbody2D.velocity.y);
+}
 
     private IEnumerator Dash()
     {
-        estado = PlayerState.Dash;
-        yield return new WaitUntil(() => Mathf.Abs(rigidbody2D.velocity.x + rigidbody2D.velocity.y) < Velocidad + MIN_DELTA);
+        DashCooldown = MAX_DASH_COOLDOWN;
+
+        estado = PlayerState.Dashing;
+        yield return new WaitUntil(() => GetVelocidadAbsoluta() <= Velocidad);
         estado = PlayerState.IDLE;
+
+        while (DashCooldown > 0)
+        {
+            yield return new WaitForSeconds(0.1f);
+            DashCooldown -= 0.1f;
+        }
     }
 }
